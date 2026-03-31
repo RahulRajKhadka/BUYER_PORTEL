161c13a0d9fcd122fc446722ea2792b3b2baef7c
RAHUL RAJ KHADKA <sirrahul9848@gmail.com>
2026-03-31 16:02:31 +0545

    prepare for deployment

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -12,11 +12,11 @@ dotenv.config();
 
 const app = express();
 
+
 app.use(cors({
-  origin:'http://localhost:5173' ,
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
 }));
-
 
 app.use(express.json());
 
